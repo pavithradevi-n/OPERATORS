bfd716fe9bdc761e829d4c4caee40cca72f8f266
pavithradevi-n <sumathipavithra1234@gmail.com>
2025-12-11 14:07:35 +0530

    add program14

---
+++ program14.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class program14 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int cube=num*num*num;
+        System.out.println(cube);
+        sc.close();
+    }
+}
