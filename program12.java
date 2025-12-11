f847d828a6f02a7b6d6212970444c7d878078a2d
pavithradevi-n <sumathipavithra1234@gmail.com>
2025-12-11 12:52:10 +0530

    add program12

---
+++ program12.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class program12 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int val=Math.abs(num);
+        System.out.println(val);
+        sc.close();
+
+    }
+    
+}
