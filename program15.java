48dda23a2f3e150cc49654751308b1cbe146995b
pavithradevi-n <sumathipavithra1234@gmail.com>
2025-12-11 14:12:17 +0530

    add program14

---
+++ program15.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class program15 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double num1=sc.nextDouble();
+        double num2=sc.nextDouble();
+        double num3=sc.nextDouble();
+        double average=(num1+num2+num3/3);
+        System.out.println(average);
+        sc.close();
+    }
+}
