4f03af53d0acdbad3099c9eb00cc986d5c009edd
pavithradevi-n <sumathipavithra1234@gmail.com>
2025-12-11 14:02:52 +0530

    add program13

---
+++ program13.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class program13 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int square=num*num;
+        System.out.println(square);
+        sc.close();
+    }
+}
