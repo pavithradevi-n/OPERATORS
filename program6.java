57bdaaac10a7c6bb704413951412a74bc84e2b7d
pavithradevi-n <sumathipavithra1234@gmail.com>
2025-12-11 12:07:26 +0530

    add program6

---
+++ program6.java
@@ -0,0 +1,13 @@
+public class program6 {
+    public static void main(String[] args) {
+        int a=8;
+        int b=0;
+        int temporary;
+        temporary=a;
+        a=b;
+        b=temporary;
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
