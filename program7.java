16e39b59fc0b46945172f566804f5862e88043f7
pavithradevi-n <sumathipavithra1234@gmail.com>
2025-12-11 12:10:08 +0530

    add program7

---
+++ program7.java
@@ -0,0 +1,12 @@
+public class program7 {
+    public static void main(String[] args) {
+        int a=3;
+        int b=8;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+         System.out.println(a);
+         System.out.println(b);
+
+    }
+}
